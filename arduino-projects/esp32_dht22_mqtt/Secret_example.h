790134d1fac581828b50130582f09eb72fab6afb
Hut <changchun.chen@growatt.com>
2026-04-24 11:40:09 +0800

    Added an example of secret file

---
+++ arduino-projects/esp32_dht22_mqtt/Secret_example.h
@@ -0,0 +1,15 @@
+char WIFI_SSID[] = "wifiname";
+char WIFI_PASSWORD[] = "wifi-password";
+
+// MQTT 配置
+const char* MQTT_SERVER = "your_broker_host";
+const uint16_t MQTT_PORT = 1883;
+const char* MQTT_USER = "mqtt_username";      // 如果需要认证
+const char* MQTT_PASSWORD = "mqtt_pass";  // 如果需要认证
+const char* MQTT_CLIENT_ID = "your id";
+
+const char* MQTT_TOPIC = "home/livingroom/env/esp32_D777EC/state";
+const char* MQTT_TOPIC_DeviceInfo = "home/livingroom/env/esp32_D777EC/metrics";
+
+const char* kDevice_Location = "home/livingroom/env/";
+const char* kDevice_Type = "esp32c3";
